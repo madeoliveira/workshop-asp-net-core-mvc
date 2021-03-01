b6032878a0725ef81df450851781de2643d0fb51
Marcio Oliveira <marcio.alves78@hotmail.com>
2021-03-01 15:02:12 -0300

    Foreign key not null (referential integrity)

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -21,6 +21,7 @@ namespace SalesWebMvc.Services
         }
         public void Insert(Seller obj)
         {
+            obj.Departament = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
